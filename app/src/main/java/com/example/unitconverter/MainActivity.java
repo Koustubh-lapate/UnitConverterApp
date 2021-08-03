ec8097ca3e782848c2fbdad480f34a7c0acdca7a
koustubh Lapate <koustubh.lapate@gmail.com>
2021-08-03 08:32:42 +0530

    Unit Converter App

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.unitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private EditText editText;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textView);
+        editText=findViewById(R.id.editText);
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                int Kg=Integer.parseInt(s);
+                double pound = 2.205*Kg;
+                textView.setText("The corresponding value in Pounds is "+ pound);
+            }
+        });
+    }
+}
